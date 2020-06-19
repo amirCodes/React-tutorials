9ddbd8b0beae8c6b03d84d915a363562fe2833e5
amirCodes <amirdanish126@gmail.com>
2020-06-19 19:11:32 +0800

    Updates

---
+++ precache-manifest.39f9c4ca7c7e7130f10ff2e91e104b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b8c8828a4418423a7341c6e8d37cdf",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "3c753d979be79239fd20",
+    "url": "/react-tutorial/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "17c7c28e5909e414c335",
+    "url": "/react-tutorial/static/js/2.59e56db0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-tutorial/static/js/2.59e56db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c753d979be79239fd20",
+    "url": "/react-tutorial/static/js/main.31071a31.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
